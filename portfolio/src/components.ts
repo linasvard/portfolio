a533d4cc7806c4e407ab674b86cc13bfc33272cf
linasvard <linajsvard@hotmail.com>
2026-03-20 16:26:01 +0100

    build: init ts file that build navbar

---
+++ portfolio/src/components.ts
@@ -0,0 +1,18 @@
+const navbar = document.querySelector('#navbar');
+if (navbar) {
+  navbar.innerHTML = `
+    <nav class="navbar-top">
+      <div class="links-navbar">
+        <a href="/index.html">Home</a>
+        <a href="#">Work</a>
+        <a href="/about-me.html">About me</a>
+        <a href="/gallery.html">Gallery</a>
+      </div>
+      <div>
+        <a class="logo-back-to-home" href="/index.html">
+            <img src="/bild.jpg" />
+        <a>
+      </div>      
+    </nav>
+  `;
+}
